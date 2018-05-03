8eba001c2a757da37453350c3c7e2e509bf6dd9d
lishimeng <alex.li@jxfinancial.cn>
2018-05-03 14:38:32 +0800

    u

---
+++ app/src/main/java/com/thingple/library/shared_context/SharedContext.java
@@ -0,0 +1,25 @@
+package com.thingple.library.shared_context;
+
+import android.content.Context;
+
+/**
+ *
+ * Created by lism on 2017/8/18.
+ */
+public class SharedContext extends PropertiesHandler {
+
+    private static SharedContext ins;
+
+    SharedContext(Context context, String name) {
+        super(context, name);
+    }
+
+    public static void init(Context context, String name) {
+        ins = new SharedContext(context, name);
+    }
+
+    public static SharedContext shareInstance() {
+        return ins;
+    }
+
+}
